da253aa654201862cf3c0dbbde875051f32ba7a5
Saidraxmonov Umar <saidraxmonovumar@MacBook-Pro.local>
2026-02-20 19:02:37 +0500

    new ones

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,9 +6,7 @@ const globalForPrisma = global as unknown as {
 
 const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
